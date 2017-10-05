53896e01e6e39885724cacf30ad6dbe66a049864
Aline Andrade Martins <andradealinem@gmail.com>
2017-10-04 22:05:05 -0300

    Update Codigo.java

--- Codigo.java
+++ Codigo.java
@@ -5,6 +5,17 @@ public class DivisaoDeNumeros {
 	public static void main(String[] args) {
 		//Criamos um objeto Scanner para capturar o que foi digitado
 		Scanner input = new Scanner(System.in);
+		
+		//Objeto para ler o nome digitado
+		Scanner ler = new Scanner(System.in);
+		//Variavel para guardar o nome digitado
+		String nome;
+		
+		//Imprime mensagem para a inserção do nome do usuario
+		System.out.println("Insira seu nome: ");
+		//Guardar o nome digitado
+		nome = ler.nextLine();
+		
 		//Imprime mensagem para a inserção do primeiro valor
 		System.out.println("Insira o valor do dividendo: ");
 		//Guarda o valor digitado pelo usuário na variável dividendo
@@ -22,11 +33,11 @@ public class DivisaoDeNumeros {
 		//Verifica se o valor do cálculo da divisão é negativo
 		else if ((dividendo / divisor < 0)) {
 			//Imprime o valor 0 caso o resultdo da divisão seja negativo
-			System.out.println("Valor encontrado: 0");
+			System.out.println(nome+", Valor encontrado: 0");
 		}
 		else {
-			//Como o divisor não é zero e o cálculo não é negativo, imprime o resultado da divisão
-			System.out.println("Valor calculado: " + dividendo / divisor);
+			//Como o divisor não é zero e o cálculo não é negativo, imprime o nome e o resultado da divisão
+			System.out.println(nome+", Valor calculado: " + dividendo / divisor);
 		}
 	}
 }
